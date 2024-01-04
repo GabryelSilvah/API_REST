52c8bb60162bab660f726a01515b818d268415d3
GabryelSilvah <dsjgabriel16@gmail.com>
2024-01-04 07:50:18 -0300

    Instanciando a classe router

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 require_once("./config/config.php");
-require_once("./model/modelUser.php");
+require_once("./model/modelUsers.php");
 require_once("./controller/getList.php");
 require_once("./controller/getName.php");
 require_once("./controller/post.php");
 require_once("./controller/status.php");
 require_once("./controller/delete.php");
 require_once("./core/router.php");
+require_once("./controller/put.php");
 
 $router = new Router;
 $router = $router->routes();
