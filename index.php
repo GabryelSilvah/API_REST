03c9435d97343a7e3b1cff54c6ae8a94eedbb707
GabryelSilvah <dsjgabriel16@gmail.com>
2024-04-13 13:26:26 -0300

    Configuração do cors

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
-require_once("./app/config/db_config.php");
-require_once("./router/router.php");
-require_once("./app/config/status.php");
-require_once("./autoload/autoload.php");
+require_once(__DIR__ . "/app/config/db_config.php");
+require_once(__DIR__ . "/router/router.php");
+require_once(__DIR__ . "/app/config/status.php");
+require_once(__DIR__ . "/autoload/autoload.php");
 
 //Controller
 
 
-//Modedel
+//mb_convert_encoding
+
+//Cors 
+header("Access-Control-Allow-Origin: http://localhosr:8007/sistema_funcionarios/");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: content-type");
+header("Content-Type: application/json charset=UTF-8");
 
 
 $router = new Router;
