851e191695353bf13d213994b81e8e5838612095
GabryelSilvah <dsjgabriel16@gmail.com>
2024-04-14 03:36:20 -0300

    Instância do Dao

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once(__DIR__ . "/app/config/db_config.php");
 require_once(__DIR__ . "/router/router.php");
 require_once(__DIR__ . "/app/config/status.php");
 require_once(__DIR__ . "/autoload/autoload.php");
+require_once(__DIR__ . "/Dao/dao_user.php");
 
 //Controller
 
@@ -10,7 +11,7 @@ require_once(__DIR__ . "/autoload/autoload.php");
 //mb_convert_encoding
 
 //Cors 
-header("Access-Control-Allow-Origin: http://localhosr:8007/sistema_funcionarios/");
+header("Access-Control-Allow-Origin: ".dns_front);
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: content-type");
 header("Content-Type: application/json charset=UTF-8");
